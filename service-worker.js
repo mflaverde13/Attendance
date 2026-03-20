66828bc3723cd193276ba55af2507e3d51fe1f81
mflaverde13 <mflaverde13@gmail.com>
2026-03-19 20:42:33 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,36 @@
-const CACHE_NAME = "attendance-v1";
-
-const urlsToCache = [
+const CACHE_NAME = "attendance-v3";
+const URLS_TO_CACHE = [
   "/Attendance/",
-  "/Attendance/index.html"
+  "/Attendance/index.html",
+  "/Attendance/manifest.json",
+  "/Attendance/icon-192.png",
+  "/Attendance/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
